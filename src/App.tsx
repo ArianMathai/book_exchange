24211381b7d778385863ab5afab501ce95da2fae
ArianMathai <123459777+ArianMathai@users.noreply.github.com>
2025-05-27 01:08:06 +0200

    added per-user data isolation

--- src/App.tsx
+++ src/App.tsx
@@ -7,7 +7,7 @@ const client = generateClient<Schema>();
 
 function App() {
 
-    const { signOut } = useAuthenticator();
+    const { signOut, user } = useAuthenticator();
   const [todos, setTodos] = useState<Array<Schema["Todo"]["type"]>>([]);
 
   useEffect(() => {
@@ -26,7 +26,7 @@ function App() {
 
   return (
     <main>
-      <h1>My todos</h1>
+        <h1>{user?.signInDetails?.loginId}'s todos</h1>
       <button onClick={createTodo}>+ new</button>
       <ul>
         {todos.map((todo) => (
@@ -43,6 +43,7 @@ function App() {
         </a>
       </div>
         <button onClick={signOut}>Sign out</button>
+
     </main>
   );
 }
